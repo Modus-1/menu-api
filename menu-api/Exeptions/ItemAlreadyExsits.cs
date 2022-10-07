fc660d6d7ed8062c9d77c3f4f5d17b52fe4cb6ae
TeunMos <teun.mos@gmail.com>
2022-10-07 14:39:02 +0200

    Added Tests

---
+++ menu-api/Exeptions/ItemAlreadyExsits.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace menu_api
+{
+    [Serializable]
+    public class ItemAlreadyExsits : Exception
+    {
+        public ItemAlreadyExsits()
+        {
+        }
+
+        public ItemAlreadyExsits(string? message) : base(message)
+        {
+        }
+
+        public ItemAlreadyExsits(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected ItemAlreadyExsits(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}

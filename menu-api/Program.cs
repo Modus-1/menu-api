5f77f1a800a3d523c226148b89b3c46275c890d1
code-syl <ejjvandelaar@gmail.com>
2022-12-06 09:49:56 +0100

    Changed CORS to accept from port 12345

--- menu-api/Program.cs
+++ menu-api/Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: AllowSpecificOrigins,
                       builder =>
                       {
-                          builder.WithOrigins("http://localhost:3000");
+                          builder.WithOrigins("http://localhost:12345");
                       });
 });
 

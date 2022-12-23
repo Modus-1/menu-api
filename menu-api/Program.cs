bac7b890f45794a1ac37edddc8f5a8e9332b3994
code-syl <ejjvandelaar@gmail.com>
2022-12-23 13:40:10 +0100

    floep

--- menu-api/Program.cs
+++ menu-api/Program.cs
@@ -1,10 +1,8 @@
-using System.Reflection;
 using menu_api;
 using menu_api.Context;
 using menu_api.Repositories;
 using menu_api.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,7 +20,9 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: AllowSpecificOrigins,
                       builder =>
                       {
-                          builder.WithOrigins(new string[]{"http://localhost:12345", "http://localhost:3002"});
+                          builder.WithOrigins("http://localhost:12345", "http://localhost:3002");
+                          builder.WithMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
+                          builder.WithHeaders("Content-Type", "Accept");
                       });
 });
 
@@ -38,20 +38,7 @@ builder.Services.AddTransient<DatabaseSeeder>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{ // uncomment to generate xml comments for Swagger
-    /*c.SwaggerDoc("v1",
-        new OpenApiInfo
-        {
-            Title = "Menu API",
-            Version = "v1"
-        }
-    );
-
-    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    c.IncludeXmlComments(xmlPath);*/
-});
+builder.Services.AddSwaggerGen();
 
 // Data Seeding start
 var app = builder.Build();
